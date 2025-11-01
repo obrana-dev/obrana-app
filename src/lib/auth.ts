2baa4aa36267e6daf3a26faf1b0ead6da1ee7121
Tomas Altrui <altruitomas@gmail.com>
2025-11-01 04:57:01 +0000

    added schema to drizzle adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,12 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { reactStartCookies } from "better-auth/react-start";
 import { db } from "@/db";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
+		schema,
 	}),
 	emailAndPassword: {
 		enabled: true,

732b8041b01aee1617e98659a627de43155076d9
Tomás Altrui <altruitomas@gmail.com>
2025-11-18 21:05:04 -0300

    add cache headers

---
+++ nitro.config.ts
@@ -0,0 +1,20 @@
+import { defineNitroConfig } from "nitropack/config";
+
+export default defineNitroConfig({
+	// Set cache headers for static assets
+	routeRules: {
+		// Cache hashed assets for 1 year (they have content hashes in filenames)
+		"/assets/**": {
+			headers: {
+				"cache-control": "public, max-age=31536000, immutable",
+			},
+		},
+		// Don't cache the HTML pages (they need to be fresh to load new asset versions)
+		"/**": {
+			headers: {
+				"cache-control": "public, max-age=0, must-revalidate",
+			},
+		},
+	},
+	compatibilityDate: "2025-11-18",
+});
